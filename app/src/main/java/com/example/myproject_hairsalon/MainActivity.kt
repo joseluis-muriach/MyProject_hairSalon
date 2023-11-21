4011cad28a968d2c0a5b13e6ae7b5141cd90183f
Jose Luis Muriach <alumur497@ieselcaminas.org>
2023-11-21 19:41:27 +0100

    changed the image fwtp

--- app/src/main/java/com/example/myproject_hairsalon/MainActivity.kt
+++ app/src/main/java/com/example/myproject_hairsalon/MainActivity.kt
@@ -22,8 +22,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //CoverTypeFace()
-                    CoverTypesOfFaces()
+                    CoverTypeFace()
+                    //CoverTypesOfFaces()
                 }
             }
         }
